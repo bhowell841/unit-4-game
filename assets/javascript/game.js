fdbbdf6a0dac07423c2e591ef57de06c54db112e
Brendan Howell <bhowell841@gmail.com>
2018-12-30 21:41:09 -0500

    numbers no longer repeat

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,9 @@ var random;     // number to reach
 var wins = 0;
 var losses = 0;
 
+var values = [];
+var number;
+
 var gem1;
 var gem2;
 var gem3;
@@ -18,13 +21,33 @@ function getNumber(){
 }
 
 // Function to give each gem a value between 1 and 12
+// function assignValues() {
+//     gem1 = Math.floor(Math.random() * 12 + 1);
+//     gem2 = Math.floor(Math.random() * 12 + 1);
+//     gem3 = Math.floor(Math.random() * 12 + 1);
+//     gem4 = Math.floor(Math.random() * 12 + 1);
+//     console.log(gem1, gem2, gem3, gem4);
+// };
+
 function assignValues() {
-    gem1 = Math.floor(Math.random() * 11 + 1);
-    gem2 = Math.floor(Math.random() * 11 + 1);
-    gem3 = Math.floor(Math.random() * 11 + 1);
-    gem4 = Math.floor(Math.random() * 11 + 1);
+    // fill a array with 4 unique values
+    for (i = 0; i < 4; i++) {
+        number = Math.floor(Math.random() * 12 + 1)
+        if (values.includes(number) === true){
+            i = i-1;
+        } else {
+            values.push(number);
+        }
+    }
+// Assign each gem a value from the Values array
+    console.log(values);
+    gem1 = values[0];
+    gem2 = values[1];
+    gem3 = values[2];
+    gem4 = values[3];
     console.log(gem1, gem2, gem3, gem4);
-};
+}
+
 
 //  Set up the click events
 function onGemClick(gemValue) {
@@ -75,6 +98,8 @@ function loseGame() {
 function reset() {
     // reset stuff here
     score = 0;
+    values = [];
+    $("#score").text(score);
     assignValues();
     getNumber();
 }
